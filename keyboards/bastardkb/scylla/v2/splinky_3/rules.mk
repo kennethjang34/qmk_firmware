aff4cf5343782b1535ea525e100764b0f3af8b6d
Junhyeok Jang <89117160+kennethjang34@users.noreply.github.com>
2024-06-08 06:44:34 +0000

    chore: keymap

--- keyboards/bastardkb/scylla/v2/splinky_3/rules.mk
+++ keyboards/bastardkb/scylla/v2/splinky_3/rules.mk
@@ -1,3 +1,11 @@
 AUDIO_SUPPORTED = no        # Audio is not supported
 
 SERIAL_DRIVER = vendor
+<<<<<<< Updated upstream
+=======
+WS2812_DRIVER = vendor
+
+KEY_OVERRIDE_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+CONSOLE_ENABLE = yes
+>>>>>>> Stashed changes
